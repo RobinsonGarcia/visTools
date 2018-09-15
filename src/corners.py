de5948d40eb9571211e6d8cd030562dbef496736
alien <rlsg.mec@hotmail.com>
2018-09-14 22:48:05 -0400

    added corners src, descriptor cell, started calibration

--- src/corners.py
+++ src/corners.py
@@ -1,119 +1,241 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import math
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 #https://www.cis.rit.edu/~cnspci/references/dip/feature_extraction/harris1988.pdf
 
-def gradientG(t,sigma):
-    return (-t/(2*math.pi*sigma**4))*math.e**(-1*(t**2)/(2*sigma**2))
+def grad(mode):
+    if mode=='fd':
+        gx = np.array([[0,0,0],[-1,0,1],[0,0,0]])
+        gy = gx.T
+        g = np.dstack((gx,gy))
+    if mode=='sobel':
+        gx = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
+        gy = gx.T
+        g = np.dstack((gx,gy))
+    if mode=='laplacian':
+        gx = np.array([[1,1,1],[1,8,1],[1,1,1]])
+        gy = gx.T
+        g = np.dstack((gx,gy))
+    return g
 
-def DOG(h,w,sigma,grad_type='x'):
+def get_im2col_indices(x_shape, field_height, field_width, p_x=1,p_y=1, stride=1):
+    # First figure out what the size of the output should be
+    _, C, H, W = x_shape
+    assert (H + 2 * p_x - field_height) % stride == 0
+    assert (W + 2 * p_y - field_height) % stride == 0
+    out_height = int((H + 2 * p_x - field_height) / stride + 1)
+    out_width = int((W + 2 * p_y - field_width) / stride + 1)
+
+    i0 = np.repeat(np.arange(field_height), field_width)
+    i0 = np.tile(i0, C)
+    i1 = stride * np.repeat(np.arange(out_height), out_width)
+    j0 = np.tile(np.arange(field_width), field_height * C)
+    j1 = stride * np.tile(np.arange(out_width), out_height)
+    i = i0.reshape(-1, 1) + i1.reshape(1, -1)
+    j = j0.reshape(-1, 1) + j1.reshape(1, -1)
+
+    k = np.repeat(np.arange(C), field_height * field_width).reshape(-1, 1)
+
+    return (k, i, j)
+
+
+
+def conv2d(im,g,stride=1,C=3):
+    im = np.repeat(im[np.newaxis,:,:],2,axis=0)
+
+    g = np.moveaxis(g,2,0)
+
+    im = im[:,np.newaxis,:,:]
+    N,_,H,W = im.shape
+    _,h,w = g.shape
+
+    stride=1
+    h_pad = int((H*(stride-1)-stride+h)/2)
+    w_pad = int((W*(stride-1)-stride+w)/2)
+
+    k,i,j = get_im2col_indices((N,1,H,W), h, w, p_x=h_pad,p_y=w_pad, stride=1)
+
+    im_padded = np.pad(im,((0,0),(0,0),(h_pad,h_pad),(w_pad,w_pad)),'mean')
+    cols = im_padded[:,k,i,j]
+
+
+    g = g.reshape((N,-1))
+
+
+    sol = np.squeeze(np.matmul(g[:,np.newaxis,:],cols))
+
+    return sol.reshape(N,H,W)
+
+def GaussianFilter(w,h,sigma):
     m = (w-1)/2
     n = (h-1)/2
     G = []
-    for j in range(w):
-        for i in range(h):
-            if grad_type=='x':
-                G.append(gradientG((i-n),sigma))
-            elif grad_type=='y':
-                G.append(gradientG((j-n),sigma))
+    for i in range(w):
+        for j in range(h):
+            G.append((1/(2*math.pi*sigma**2))*math.e**(-1*((i-m)**2+(j-n)**2)/(2*sigma**2)))
 
-    return np.array(G).reshape(w,h)
+    return np.array(G).reshape(w,h)/np.sum(np.array(G))
 
-def plot(img,**kwargs):
-    plt.figure()
-    plt.imshow(img,interpolation="none",**kwargs)
-    plt.axis('off')
+def Reduce(im,k):
+    for j in range(k):
+        H,W = im.shape
 
-def compute_H(f,Ix,Iy,h=3,w=3,k=1):
-    x_2 = Ix**2
-    y_2 = Iy**2
-    xy = Ix*Iy
+        Dx = np.zeros((int((H+H%2)/2),H))
+        Dx[np.arange(int((H+H%2)/2)),np.arange(0,H,2)]=1
 
-
-    pts = np.argwhere(f)
-    H,W = f.shape
-    H = int(H)
-    W = int(W)
-
-    x_pad = int(0.5*((H-1)*k-H+h))
-    y_pad = int(0.5*((W-1)*k-W+w))
+        Dy = np.zeros((int((W+W%2)/2),W))
+        Dy[np.arange(int((W+W%2)/2)),np.arange(0,W,2)]=1
+        im = (Dx.dot(im)).dot(Dy.T)
+    return im
 
 
-    Ix_pad = np.pad(Ix,((y_pad,y_pad),(x_pad,x_pad)),mode='constant')
-    Iy_pad = np.pad(Iy,((y_pad,y_pad),(x_pad,x_pad)),mode='constant')
-
-    l1 = []
-    l2 = []
-    R = []
-    for m in range(int(Ix_pad.shape[0]-h)+1):
-        for n in range(int(Ix_pad.shape[1]-w+1)):
-
-            x = np.sum(Ix_pad[m:m+w,n:n+h]**2)
-            y = np.sum(Iy_pad[m:m+w,n:n+h]**2)
-            xy = np.sum(Ix_pad[m:m+w,n:n+h]*Iy_pad[m:m+w,n:n+h])
-            H_ = np.array([[x,xy],[xy,y]])
-            _,s,_ = np.linalg.svd(H_)
-
-            l1.append(s[-1])
-            l2.append(s[0])
-
-    return (np.array(l1).reshape(H,W),np.array(l2).reshape(H,W))
+class Harris_detector:
+    def __init__(self,img):
+        self.img = img
 
 
-def conv2d_loop(f,gx,gy,k=1,**kwargs):
-    w,h = gx.shape
-    W,H = f.shape
+    def solve(self,solve=False,blur_window=(3,3),deriv_type='fd',
+                                          sig=1,Red=2,sharp=False,
+                                          blur=True,
+                         second_moment_window=(3,3),
+                        clusters=None,
+                        th=0.03):
 
-    gx = np.rot90(gx,2)
-    gy = np.rot90(gy,2)
+        '''gaussian_derivatives param:'''
+        self.blur_window = blur_window
+        self.deriv_type= deriv_type
+        self.sig = sig
+        self.Red= Red
+        self.sharp = sharp
+        self.blur = blur
+        self.second_moment_window=second_moment_window
 
-    x_pad = int(0.5*((W-1)*k-W+w))
-    y_pad = int(0.5*((H-1)*k-H+w))
-
-
-    f_pad = np.pad(f,((y_pad,y_pad),(x_pad,x_pad)),mode='constant')
-
-    Ix = []
-    Iy = []
-    for m in range(int(f_pad.shape[0]-w)+1):
-        for n in range(int(f_pad.shape[1]-h+1)):
-            fgx = np.multiply(f_pad[m:m+w,n:n+h],gx)
-            sum_fgx = np.sum(fgx,axis=(0,1))
-            Ix.append(sum_fgx)
-            fgy = np.multiply(f_pad[m:m+w,n:n+h],gy)
-            sum_fgy = np.sum(fgy,axis=(0,1))
-            Iy.append(sum_fgy)
-
-    return np.array(Ix).reshape(W,H),np.array(Iy).reshape(W,H)
+        '''thresholding parameters'''
+        self.th = th
+        self.clusters = clusters
 
 
-class Harris:
-    def __init__(self):
-        self.Mx = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
-        self.My = np.rot90(self.Mx)
+        self.compute()
 
-    def solve(self,bw_im,th=0.05,h=3,w=3,gaussian=0,sigma=1):
-        self.bw_im = bw_im
-        self.Ix,self.Iy = conv2d_loop(bw_im,self.Mx,self.My,k=1)
 
-        l1,l2 = compute_H(bw_im,self.Ix,self.Iy,h,w)
+    def compute(self):
 
-        R = l2*l1-.03*(l2+l1)**2
+        I = self.get_gaussian_derivatives(self.img)
+        H = self.get_H(I)
+        self.R = self.get_corner_response(H)
+        self.apply_threshold()
 
-        #plot(R,**{'cmap':'gray'})
-        self.l1 = l1
-        self.l2 = l2
-        self.R = R
 
-        pts = np.nonzero(self.R[:,:]>th*self.R.max())
-        plt.imshow(bw_im,**{'cmap':'gray'})
-        plt.scatter(pts[1],pts[0],s=1,**{'color':'red'})
+    def get_gaussian_derivatives(self,img):
+        Red=self.Red
+        sharp=self.sharp
+        blur=self.blur
+        sig=self.sig
+        h,w=self.blur_window
+        deriv_type=self.deriv_type
+
+        self.im = plt.imread(img)
+        self.im = Reduce(np.mean(self.im,axis=2),Red)
+        self.im_shape = self.im.shape
+
+        if sharp==True:
+            g = np.array([[1,1,1],[1,-9,1],[1,1,1]])
+            g = np.dstack((g,g.T))
+            im = conv2d(self.im,g)[0]
+
+        if blur==True:
+            G = GaussianFilter(h,w,sig)
+            G = np.dstack((G,G))
+            im = conv2d(self.im,G)[0]
+        self.im = im
+        g = grad(deriv_type)
+        R = conv2d(im,g)
+        self.Sx = R[0]
+        self.Sy = R[1]
+
+        rho = np.arctan2(self.Sy,self.Sx)
+        rho[rho<0]+=2*math.pi
+        self.rho = rho
+
+        self.S = np.sqrt(np.sum(R**2,axis=0))
+        self.n = np.divide(R,self.S[np.newaxis,:,:],out=np.zeros_like(R),where=self.S!=0)
+        return R
+
+    '''Second moment matrix'''
+    def get_H(self,R):
+        Ix,Iy = R
+        Ixy = Ix*Iy
+        H_ = np.stack((Ix**2,Ixy,Ixy,Iy**2))
+
+        N,H,W = H_.shape
+        x_shape = (0,N,H,W) #image shape
+        field_height,field_width = self.second_moment_window #Gaussian window
+
+        stride=1
+        h_pad = int((H*(stride-1)-stride+field_height)/2)
+        w_pad = int((W*(stride-1)-stride+field_width)/2)
+
+        k,i,j = get_im2col_indices(x_shape, field_height, field_width, p_x=h_pad,p_y=w_pad, stride=1)
+
+        H_padded = np.pad(H_,((0,0),(h_pad,h_pad),(w_pad,w_pad)),'mean')
+
+        cols = H_padded[k,i,j]
+
+        H_ = np.sum(cols.reshape(4,field_height*field_width,-1),axis=1)
+        H_ = H_.reshape(2,2,-1)
+
+        H_ = np.moveaxis(H_,2,0)
+        return H_
+
+    def get_corner_response(self,H_):
+        U,S,V = np.linalg.svd(H_)
+        alpha=0.04
+        R = S[:,0]*S[:,1] - alpha*(S[:,0]+S[:,1])**2
+        return R
+
+    def apply_threshold(self):
+        th = self.th
+        cluster = self.clusters
+        H,W = self.im_shape
+        R_ = np.clip(self.R,0,None)
+        R_ = (R_ - R_.min())/(R_.max()-R_.min())
+
+
+        R_ = R_.reshape(H,W)
+
+        R_[R_<th]=0
+
+        R_ = self.nonMaxSup(R_)
+
+        xk = np.argwhere(R_)
+
+        if cluster !=None:
+            kmeans = KMeans(n_clusters=cluster, random_state=0).fit(xk)
+
+            xk = kmeans.cluster_centers_
+
+        plt.scatter(xk[:,1],xk[:,0],c='r',s=10)
+        plt.imshow(self.im,**{'cmap':'gray'})
         plt.show()
+        self.xk = xk
 
-    def apply_th(self,th):
-        bw_im = self.bw_im
+    def nonMaxSup(self,S):
+        n = self.n
+        S_padded = np.pad(S,((1,1),(1,1)),'mean')
+        kp = np.argwhere(S)
 
-        pts = np.nonzero(self.R[:,:]>th*self.R.max())
-        plt.imshow(bw_im,**{'cmap':'gray'})
-        plt.scatter(pts[1],pts[0],s=1,**{'color':'red'})
-        plt.show()
+        n0 = n[:,kp[:,0],kp[:,1]].T
+
+        q0 = kp+n0*1/(2*math.sin(math.pi/8))
+        q1 = kp-n0*1/(2*math.sin(math.pi/8))
+
+        q0 = np.round(q0).astype(int)
+        q1 = np.round(q1).astype(int)
+
+        maxs = (S[kp[:,0],kp[:,1]]>S_padded[q0[:,0],q0[:,1]])| (S[kp[:,0],kp[:,1]]>S_padded[q1[:,0],q1[:,1]])
+
+        maxs = kp[maxs]
+        new_S = np.zeros_like(S)
+        new_S[maxs[:,0],maxs[:,1]]=1
+        return new_S
